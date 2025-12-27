2b166a6346620bd32a92c78399a0609266e60a5a
121ShoryaSingh <shoryasingh810@gmail.com>
2025-12-27 21:25:41 +0530

    seventh try

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './../styles/App.css';
-import Step1 from './Step1';
-import Step2 from './Step2';
-import Step3 from './Step3';
+import Step from './Step';
 
 const App = () => {
   const [formData, setFormData] = useState({
@@ -29,32 +27,14 @@ const App = () => {
 
   return (
     <form>
-      {currentStep === 1 && (
-        <Step1
-          id="step1"
-          handleNextStep={handleNextStep}
-          handleFormData={handleFormData}
-          formData={formData}
-        />
-      )}
-      {currentStep === 2 && (
-        <Step2
-          id="step2"
-          handleNextStep={handleNextStep}
-          handleFormData={handleFormData}
-          handlePreviousStep={handlePreviousStep}
-          formData={formData}
-        />
-      )}
-      {currentStep === 3 && (
-        <Step3
-          id="step3"
-          handleNextStep={handleNextStep}
-          handleFormData={handleFormData}
-          handlePreviousStep={handlePreviousStep}
-          formData={formData}
-        />
-      )}
+      <Step
+        id="step1"
+        handleNextStep={handleNextStep}
+        handleFormData={handleFormData}
+        formData={formData}
+        currentStep={currentStep}
+        handlePreviousStep={handlePreviousStep}
+      />
     </form>
   );
 };
---
+++ src/components/Step.js
@@ -0,0 +1,99 @@
+import React from 'react';
+
+export default function Step({
+  handleNextStep,
+  handleFormData,
+  formData,
+  handlePreviousStep,
+  currentStep,
+}) {
+  const handleSubmit = () => {
+    handleNextStep();
+  };
+
+  return (
+    <>
+      {currentStep === 1 && (
+        <div id="step1">
+          <input
+            id="first_name"
+            type="text"
+            value={formData.firstName}
+            onChange={(e) => handleFormData('firstName', e.target.value)}
+          />
+          <input
+            id="last_name"
+            type="text"
+            value={formData.lastName}
+            onChange={(e) => handleFormData('lastName', e.target.value)}
+          />
+
+          <button
+            type="button"
+            onClick={handleSubmit}
+          >
+            Next
+          </button>
+        </div>
+      )}
+      {currentStep === 2 && (
+        <div id="step2">
+          <input
+            id="model"
+            type="text"
+            value={formData.carModel}
+            onChange={(e) => handleFormData('carModel', e.target.value)}
+          />
+          <input
+            id="car_pricee"
+            type="text"
+            value={formData.carPrice}
+            onChange={(e) => handleFormData('carPrice', e.target.value)}
+          />
+          <button
+            type="button"
+            onClick={() => {
+              handlePreviousStep();
+            }}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleNextStep();
+            }}
+          >
+            Next
+          </button>
+        </div>
+      )}
+
+      {currentStep === 3 && (
+        <div id="step3">
+          <input
+            id="card_info"
+            type="text"
+            value={formData.cardInfo}
+            onChange={(e) => handleFormData('cardInfo', e.target.value)}
+          />
+          <input
+            id="expiry_date"
+            type="text"
+            value={formData.expiryDate}
+            onChange={(e) => handleFormData('expiryDate', e.target.value)}
+          />
+          <button
+            type="button"
+            onClick={() => {
+              handlePreviousStep();
+            }}
+          >
+            Previous
+          </button>
+          <button>Submit</button>
+        </div>
+      )}
+    </>
+  );
+}
